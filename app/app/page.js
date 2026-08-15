546837ade9e8d4af875bb249ede175d81e1b1ad6
akshayshingala102001-afk <akshayshingala102001@gmail.com>
2026-08-15 19:24:57 +0530

    Create page.js

---
+++ app/app/page.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Home() {
+  return (
+    <main style={{ padding: 24, fontFamily: "Arial, sans-serif" }}>
+      <h1>Welcome</h1>
+      <p>Telegram Mini App is working.</p>
+    </main>
+  );
+}
